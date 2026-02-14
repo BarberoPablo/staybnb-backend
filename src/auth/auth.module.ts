51829822728954e78860fee61bc841f6c8de467b
Pablo <pablobarbero220@gmail.com>
2026-02-14 23:45:39 +0000

    fix/SB-54

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthController } from './auth.controller';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [AuthController],
   providers: [AuthGuard, AuthService],
   exports: [AuthGuard, AuthService],
 })
